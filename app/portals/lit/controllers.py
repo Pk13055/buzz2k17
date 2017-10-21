43404f41098499f306b80a565ffde8e4e115e2c5
Anukul Sangwan <anukulsangwan@icloud.com>
2017-10-22 05:08:01 +0530

    Update portal UI

--- app/portals/lit/controllers.py
+++ app/portals/lit/controllers.py
@@ -1,15 +1,14 @@
-from flask import request, render_template, Blueprint
+from flask import request, render_template, Blueprint, flash, redirect, url_for
 from flask_cas import login_required
+from time import time
 
 from app import cas, db
 from app.users.controllers import current_user
 from app.users.models import User
 from .models import Question
 
-
-mod_lit = Blueprint('lit', __name__)
-
-portal = 'Lit'
+portal = 'lit_quiz'
+mod_lit = Blueprint(portal, __name__)
 
 
 @mod_lit.route('/', methods=['GET', 'POST'])
@@ -17,23 +16,32 @@ portal = 'Lit'
 def route_index():
     u = current_user()
     question = Question.query.get(u.lit_q)
+    if question is None:
+        flash('You have finished the quiz successfully! :D', 'alert-success')
+        return redirect(url_for('users.route_index'))
     score = u.lit_s
     if request.method == 'GET':
         return render_template('question.html', question=question, score=score,
-                               portal=portal)
+                               portal=portal, user=u.email)
     else:
         response = request.form['response']
         if response == question.answer:
             u.lit_q += 1
             u.lit_s += 100
+            u.lit_t = time()
             db.session.commit()
-            return 'Your answer is correct!!!'
+            flash('Your answer is correct!', 'alert-success')
         else:
-            return 'Try again!!! :('
+            flash('Try again, your answer is incorrect :(', 'alert-danger')
+        return redirect(url_for('lit_quiz.route_index'))
 
 
 @mod_lit.route('/leaderboard')
+@login_required
 def route_leaderboard():
+    u = current_user()
     users = User.query.all()
-    sorted_users = sorted(users, key=lambda u: (u.lit_s, -u.lit_t), reverse=True)
-    return render_template('leaderboard.html', users=sorted_users, portal=portal)
+    s_users = sorted(users, key=lambda u: (u.lit_s, -u.lit_t), reverse=True)
+    u_users = [(u.email, u.lit_s) for u in s_users]
+    return render_template('leaderboard.html', users=enumerate(u_users),
+                           portal=portal, user=u.email, score=u.lit_s)
